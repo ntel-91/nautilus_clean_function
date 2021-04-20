486055a8ec6f4c1530b1fadb3dddfedffddc199f
ntel-91 <ntelenson@gmail.com>
2021-04-20 16:33:43 -0400

    complete

---
+++ index.js
@@ -0,0 +1,28 @@
+function f()  {
+    if (!A) {
+        s();
+        return false;
+    }
+    z();
+
+    if (!B) {
+        t();
+        return false;
+    }
+    y();
+
+    if (!C) {
+        u();
+        return false;
+    }
+    x();
+
+    if (!D) {
+        v();
+        return false;
+    }
+    w();
+
+    return true
+
+}
